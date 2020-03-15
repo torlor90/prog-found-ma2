c1213d1941de57f3413aa23198775fa4ac1de236
torlor90 <60573252+torlor90@users.noreply.github.com>
2020-03-15 16:35:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// question 1
+
+var petArray = ["cat", "cow", "dog"];
+
+// question 2
+
+console.log(petArray[1]);
+
+//question 3
+
+petArray.push("sheep");
+
+// question 4
+
+console.log(petArray.length);
+
+// question 5
+
+var catObject = {
+    name: "Simba",
+    colour: "red",
+    age: 2
+};
+
+// question 6
+
+console.log(catObject.age);
+
+// question 7
+
+var catArray = [
+{
+    name: "Simba",
+    colour: "red",
+    age: 2
+}
+];
+
+// question 8
+
+for (i = 0; i < catArray.length; i++) {
+    console.log(catArray[i]);
+};
+
+// question 9
+
+function logToConsole(cutePet) {
+    console.log(cutePet);
+}
+logToConsole("duck");
+
+// question 10
+
+logToConsole("donkey");
